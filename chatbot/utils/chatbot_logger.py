ad2fd518472e0d8bfa1f6748f7ce2301385b0618
DESKTOP-5NUN1M7\bhjeon <bhjeon@imbu.co.kr>
2025-11-24 17:47:39 +0900

    36차 테스트 커밋

---
+++ chatbot/utils/chatbot_logger.py
@@ -0,0 +1,247 @@
+"""
+* 챗봇 커스텀 로거 유틸 (util)
+코드 리뷰 참고 URL - https://chatgpt.com/c/68f9886c-6080-8320-9657-836cde5674f8
+"""
+
+# 1. 먼저 공통 모듈(module) import
+from commons import chatbot_helper   # 챗봇 전용 도움말 텍스트
+
+# 2. 나머지 모듈(module) import 
+import inspect
+import os   # 상위 호출자 파일명 구하는 용도
+from datetime import datetime
+from zoneinfo import ZoneInfo    # 대한민국 표준시 포맷 변환 용도
+
+"""
+* 로그 레벨 종류 
+|   Level   |   Value   |   When to use
+|   DEBUG   |     10    | (주로 문제 해결을 할때 필요한) 자세한 정보. - 개발 과정에서 오류 원인 파악하고자 할 때 사용
+|   INFO    |     20    | 작업이 정상적으로 작동하고 있다는 메시지. 
+|  WARNING  |     30    | 예상하지 못한 일이 발생하거나, 발생 가능한 문제점을 명시. (e.g 'disk space low') 작업은 정상적으로 진행.
+|   ERROR   |     40    | 프로그램이 함수를 실행하지 못 할 정도의 심각한 문제.
+|  CRITICAL |     50    | 프로그램이 동작할 수 없을 정도의 심각한 문제. 
+""" 
+
+DEBUG = 'DEBUG'
+INFO = 'INFO'
+WARNING = 'WARNING'
+ERROR = 'ERROR'
+CRITICAL = 'CRITICAL'
+
+_kst = None   # ZoneInfo 클래스 (global) 전역변수 객체
+
+# def _test(level: str, msg: str) -> None:
+#     """
+#     Description: 로그 오류 테스트 함수
+
+#     Parameters: level - 로그 레벨
+#                 msg - 로그 메시지
+
+#     Returns: 없음.
+#     """
+    
+#     current_frame = inspect.currentframe()   # 현재 실행 중인 프레임 의미
+#     file_name = os.path.basename(current_frame.f_code.co_filename)
+#     function_name = current_frame.f_code.co_name
+#     lineno = current_frame.f_lineno
+#     print("[%s] [%s] [%s | %s - L%d]: %s" %(ERROR, datetime.now(_kst).strftime(chatbot_helper._datefmt), file_name, function_name, lineno, "로그 오류 테스트"))
+#     _logWrite(level, msg)
+
+def _initZoneInfo() -> None:
+    """
+    Description: ZoneInfo 클래스 (global) 전역변수 객체 초기화 (1회만 실행)
+                 한국 표준 시간대("Asia/Seoul") 변경되지 않기 때문에 1회만 실행.
+
+    Parameters: 없음.
+
+    Returns: 없음.
+    """
+
+    global _kst
+ 
+    if None is _kst:   # (global) 전역변수 객체(_kst)에 할당된 값이 존재하지 않은 경우
+        # zoneinfo 파이썬 라이브러리 사용하여 로그 출력시 대한민국 표준시로 출력 (2025.06.13 minjae)
+        # 참고 URL - https://docs.python.org/ko/3.9/library/zoneinfo.html#module-zoneinfo
+        # 참고 2 URL - https://wikidocs.net/236273
+        # 참고 3 URL - https://chatgpt.com/c/684b79a8-8c20-8010-9d14-41ab28f12747
+        _kst = ZoneInfo("Asia/Seoul")   # 대한민국 표준시로 설정할 수 있도록 ZoneInfo 클래스 (global) 전역변수 객체 _kst 생성 
+        current_frame = inspect.currentframe()   # 현재 실행 중인 프레임 의미
+        file_name = os.path.basename(current_frame.f_code.co_filename)
+        function_name = current_frame.f_code.co_name
+        lineno = current_frame.f_lineno        
+        print("[%s] [%s] [%s | %s - L%d]: %s" %(INFO, datetime.now(_kst).strftime(chatbot_helper._datefmt), file_name, function_name, lineno, "[테스트] ZoneInfo 클래스 (global) 전역변수 객체 _kst - 생성 완료!"))   # 현재 시간 새로 생성 및 반환
+
+def _callerInfo() -> tuple[str, str, int]:
+    """
+    Description: 상위 호출자 파일명 (file_name), 함수명 (function_name), 라인 번호 (lineno) 가져오기
+                 튜플(tuple) 객체 사용하여 값 반환
+                 참고 URL - https://dojang.io/mod/page/view.php?id=2340
+                 참고 2 URL - https://wikidocs.net/3133
+                 참고 3 URL - https://wikidocs.net/92538
+
+    Parameters: 없음.
+
+    Returns: file_name - 상위 호출자 파일명
+             function_name - 함수명
+             lineno - 라인 번호
+    """
+
+    # 상위 호출자 파일명, 함수명, 라인 번호 가져오기 
+    # 참고 URL - https://docs.python.org/ko/3.8/library/inspect.html
+    # 참고 2 URL - https://louky0714.tistory.com/144
+    # 참고 3 URL - https://wikidocs.net/3717
+    # 참고 4 URL - https://visit-my.blog/2024/02/11/%ED%8C%8C%EC%9D%B4%EC%8D%AC-%ED%98%84%EC%9E%AC-%ED%95%A8%EC%88%98-%EC%9D%B4%EB%A6%84%EA%B3%BC-%ED%98%B8%EC%B6%9C%ED%95%9C-%ED%95%A8%EC%88%98-%EC%9D%B4%EB%A6%84-%ED%99%95%EC%9D%B8-%ED%95%98%EA%B8%B0/
+    # 참고 5 URL - https://dev.to/atifwattoo/inspectstack-in-python-with-examples-l4m
+    # frame = inspect.currentframe().f_back.f_back.f_back   # 3단계 위 상위 호출자 정보 가져오기
+    stack = inspect.stack()   # 현재 stack 전체 가져오기
+
+    # stack 구조 예시(인덱스 번호 기준): [3]번째 프레임이 상위 호출자이다.
+    # stack[0] _callerInfo
+    # stack[1] _logWrite
+    # stack[2] debug / info / warning / error / critical
+    # stack[3] [2]번 함수들을 직접 호출한 상위 호출자
+
+    # 그리디 알고리즘 (Greedy Algorithm) - 탐욕법이라고 불리며, 현재 상황에서 지금 당장 좋은 것만 고르는 방법이다.
+    # 참고 URL - https://youtu.be/5OYlS2QQMPA?si=LzCRpZvGmEXI5Ean
+    # 참고 2 URL - https://youtu.be/_TG0hVYJ6D8?si=j85mnzUabJeClsoQ
+
+    for frame_info in stack:   # 호출 stack 역순으로 순회 및 chatbot_logger.py 모듈(module) 밖 상위 호출자 자동 탐색.
+        module = inspect.getmodule(frame_info.frame)  # 해당 프레임 객체가(frame_info.frame) 어느 모듈(module)에서 생성되었는지 추적
+        file_name = os.path.basename(frame_info.filename)   # 상위 호출자 함수가 속한 파일 이름
+        cur_file_name = os.path.basename(__file__)   # 현재 실행 중인 함수(_callerInfo) 코드가 속한 파일 이름 변수(어트리뷰트) (chatbot_logger.py)
+
+        # [테스트] [오류 케이스] chatbot_logger.py 모듈(module) 밖 상위 호출자 찾기 불가! 
+        # module = None 
+        # cur_file_name = os.path.basename(__file__)   # 현재 실행 중인 함수(_callerInfo) 코드가 속한 파일 이름 변수(어트리뷰트) (chatbot_logger.py)
+        # file_name = cur_file_name  # 상위 호출자 함수가 속한 파일 이름 == 현재 실행 중인 함수(_callerInfo) 코드가 속한 파일 이름
+        
+        # 파이썬 __name__ 변수(어트리뷰트) - 현재 모듈(module)의 이름을 담고 있는 내장 변수(어트리뷰트)
+        # 참고 URL - https://docs.python.org/ko/3.7/reference/import.html?highlight=__name__#__name__
+        # 참고 2 URL - https://wikidocs.net/195615
+        
+        # ***** 핵심 조건 ***** 
+        # (1) module 존재하고 module 이름(module.__name__) chatbot_logger 아님
+        # (2) module None 이고 파일명 chatbot_logger.py 아님
+        if (module and module.__name__ != __name__) or (None is module and file_name != cur_file_name):   # chatbot_logger.py 내부 호출 건너띄기.
+            return (
+                file_name,
+                frame_info.function,
+                frame_info.lineno
+            )
+
+    return ("unknown_file", "unknown_function", -1)  # chatbot_logger.py 모듈(module) 밖 상위 호출자 찾기 불가!
+
+def _formatTime() -> str:
+    """
+    Description: 현재 날짜 및 시간을 대한민국 표준시 포맷된 문자열로 가져오기
+
+    Parameters: 없음.
+
+    Returns: datetime.now(_kst).strftime(chatbot_helper._datefmt) - 현재 날짜 및 시간을 대한민국 표준시로 포맷된 문자열
+    """
+        
+    global _kst
+
+    _initZoneInfo()
+ 
+    # 현재 날짜와 시간을 특정 포맷으로 변환 (2025.03.27 minjae)
+    # 참고 URL - https://wikidocs.net/269063
+    return datetime.now(_kst).strftime(chatbot_helper._datefmt)   # 매번 현재 시간 새로 생성 및 반환 
+ 
+def _logWrite(level: str, msg: str) -> None:
+    """
+    Description: 레벨별 공통 로그 기록 출력 
+                 공통 로그 출력 함수 기능 구현 (2025.09.22 minjae) 
+                 참고 URL - https://chatgpt.com/c/6847d34f-9700-8010-92fb-2d063eff183a
+
+    Parameters: level - 로그 레벨
+                msg - 로그 메시지
+
+    Returns: 없음.
+    """
+
+    time_stamp = _formatTime()
+    (file_name, function_name, lineno) = _callerInfo()
+
+    # 파이썬 문자열 포맷팅(String Formatting)
+    # 참고 URL - https://docs.python.org/ko/3/library/string.html#format-string-syntax
+    # 참고 2 URL - https://wikidocs.net/78952
+    # 참고 3 URL - https://wikidocs.net/13
+    # 참고 4 URL - https://dojang.io/mod/page/view.php?id=2300
+    # TODO: 오류 메시지 "/var/task/modules/chatbot_logger.py:170: SyntaxWarning: "is" with a literal. Did you mean "=="?" 출력되어 (기존) is 연산자 -> (변경) == 연산자 처리 완료 (2025.10.27 minjae)
+    # if "unknown_file" is file_name or "unknown_function" is function_name or -1 is lineno:   # 상위 호출자 존재 안 하는 경우
+    if "unknown_file" == file_name or "unknown_function" == function_name or -1 == lineno:   # 상위 호출자 존재 안 하는 경우
+        print("[%s] [%s] [%s | %s - L(%d)]: %s" %(level, time_stamp, file_name, function_name, lineno, 'chatbot_logger.py 모듈(module) 밖 상위 호출자 찾기 불가!'))
+    else:   # 상위 호출자 존재 하는 경우
+        print("[%s] [%s] [%s | %s - L%d]: %s" %(level, time_stamp, file_name, function_name, lineno, msg))
+
+def debug(msg: str) -> None:
+    """
+    Description: DEBUG 로그 기록 출력 
+
+    Parameters: msg - 로그 메시지 
+
+    Returns: 없음.     
+    """
+
+    # _test(DEBUG, msg)
+    _logWrite(DEBUG, msg)
+
+def info(msg: str) -> None:
+    """
+    Description: INFO 로그 기록 출력
+
+    Parameters: msg - 로그 메시지
+
+    Returns: 없음.
+    """
+
+    _logWrite(INFO, msg)
+
+def warning(msg: str) -> None:
+    """
+    Description: WARNING 로그 기록 출력
+
+    Parameters: msg - 로그 메시지
+
+    Returns: 없음.
+    """
+
+    _logWrite(WARNING, msg)
+
+def error(msg: str) -> None:
+    """
+    Description: ERROR 로그 기록 출력
+
+    Parameters: msg - 로그 메시지
+
+    Returns: 없음.
+    """
+
+    _logWrite(ERROR, msg)
+
+def critical(msg: str) -> None:
+    """
+    Description: CRITICAL 로그 기록 출력
+
+    Parameters: msg - 로그 메시지
+
+    Returns: 없음.
+    """
+
+    _logWrite(CRITICAL, msg)
+
+"""
+*** 참고 ***
+*** 파이썬 문서 ***
+* Type Hints
+참고 URL - https://docs.python.org/ko/3.14/library/typing.html
+참고 2 URL - https://peps.python.org/pep-0484/
+참고 3 URL - https://devpouch.tistory.com/189
+참고 4 URL - https://supermemi.tistory.com/entry/Python-3-%ED%8C%8C%EC%9D%B4%EC%8D%AC%EC%97%90%EC%84%9C-%EC%9D%98%EB%AF%B8%EB%8A%94-%EB%AC%B4%EC%97%87%EC%9D%BC%EA%B9%8C-%EC%A3%BC%EC%84%9D
+
+* 패키지, 모듈(module) 
+참고 URL - https://docs.python.org/ko/3.13/tutorial/modules.html
+참고 2 URL - https://wikidocs.net/1418
+참고 3 URL - https://dojang.io/mod/page/view.php?id=2450
+"""
---
+++ chatbot/utils/log.py
@@ -0,0 +1,123 @@
+"""
+* 챗봇 전역 로그 유틸 (util)
+코드 리뷰 참고 URL - https://chatgpt.com/c/69118414-a588-8323-be5b-b0362b312cfa
+
+파이썬 logging 라이브러리 사용해서 전역 로그 객체(logger) 및 로그 기록 기능 구현 (2025.09.18 minjae)
+참고 URL - https://claude.ai/chat/8fc1ceeb-fe95-4d1b-8517-ecec83beb3f2
+"""
+
+# 1. 공통 모듈 먼저 import
+from commons import chatbot_helper   # 챗봇 전용 도움말 텍스트
+
+# 2. singleton 모듈 import
+from modules.singleton import KSTFormatter   # 싱글톤(singleton) 패턴
+
+# 3. 나머지 모듈 import
+import logging, os, sys
+
+def init_logger(name: str = "chatbot_logger") -> logging.Logger:
+    """
+    Description: 챗봇 전역 로그 객체 (logger) 생성 및 초기화
+
+    Parameters: name - 챗봇 전역 로그 객체 (logger) 이름 (non-default value parameter)
+                참고 URL - https://docs.python.org/ko/3/glossary.html#term-parameter
+
+    Returns: logger - 챗봇 전역 로그 객체
+    """
+
+    # logger = logging.getLogger()   # 최상위 로그 객체 (root) 가져오기
+    logger = logging.getLogger(name)   # 최상위 로그 객체 (root) 대신 특별한 이름 ("chatbot_logger")의 전역 로그 객체 (logger) 생성
+    if logger.handlers: return logger   # 이미 초기화 된 경우 중복 방지 (handlers 중복 등록 검증)
+
+    log_level = os.getenv("LOG_LEVEL", "INFO").upper()   # 아마존 웹서비스 람다 함수 (AWS Lambda Function) 환경 변수 key ("LOG_LEVEL")의 값이 존재하면 문자열 (영어 대문자)로 반환하고, 없으면 기본값 ("INFO") 반환
+    # logger.setLevel(logging.INFO)   # 로그 레벨 INFO 레벨 이상으로 고정
+    logger.setLevel(getattr(logging, log_level, logging.INFO))   # getattr 함수 사용해서 logging.log_level 속성 값 반환 (해당 속성 값이 없는 경우 default 속성 값 logging.INFO 반환)
+    logger.propagate = False   # 아마존 웹서비스 람다 함수 (AWS Lambda Function)의 최상위 로그 객체 (root)와 분리하기 위해 propagate 속성(property) 값을 False로 설정 (False일 경우, chatbot_logger의 로깅 메시지가 최상위 로그 객체(root)의 처리기로 전달되지 않는다.)
+
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)   # 기존 핸들러 모두 제거
+
+    # formatter 생성 (로그 기록/출력/저장에 사용할 날짜 + 로그 메시지)
+    formatter = KSTFormatter("[%(levelname)s] [%(asctime)s] [%(filename)s | %(funcName)s - L%(lineno)d]: %(message)s")   # 싱글톤 (singleton) 클래스 (KSTFormatter)의 부모 클래스 (logging.Formatter) 초기화자 super().__init__(*args, **kwargs) 실행 시 파라미터 datefmt 전달 인자 값이 None일 경우 로그 기록 형식 기본 값 할당 (default_time_format = '%Y-%m-%d %H:%M:%S')
+
+    streamHandler = logging.StreamHandler(sys.stdout)   # 콘솔 출력용 handler 생성 (streamHandler)
+    streamHandler.setFormatter(formatter)   # handler formatter 설정(할당)
+    logger.addHandler(streamHandler)   # 전역 로그 객체 (logger)에 handler 추가 (로그 기록시 해당 handler 적용)
+
+    if "true" == os.getenv("ENABLE_FILE_LOG", "false").lower():
+        fileHandler = logging.FileHandler(chatbot_helper._botlog_file_path)   # 파일 기록용 handler 생성 (fileHandler) - 아마존 웹서비스 람다 함수 (AWS Lambda Function)에 로그 기록할 파일 이름(경로) "/tmp/botlog.txt" 지정 (파일 이름은 다른 것으로 변경해도 된다.)
+        fileHandler.setFormatter(formatter)   # handler formatter 설정(할당)
+        logger.addHandler(fileHandler)   # 전역 로그 객체 (logger)에 handler 추가 (로그 기록시 해당 handler 적용)
+
+    return logger
+
+logger = init_logger()   # 챗봇 전역 로그 객체 생성 및 초기화
+
+"""
+*** 참고 ***
+*** 파이썬 문서 ***
+* os.getenv
+참고 URL - https://docs.python.org/ko/3/library/os.html#os.getenv
+
+* getattr
+참고 URL - https://docs.python.org/ko/3.10/library/functions.html#getattr
+
+* 패키지, 모듈
+참고 URL - https://docs.python.org/ko/3.13/tutorial/modules.html
+참고 2 URL - https://wikidocs.net/1418
+참고 3 URL - https://dojang.io/mod/page/view.php?id=2450
+
+* logging 모듈 사용하여 로그 기록하기
+참고 URL - https://docs.python.org/ko/3/library/logging.html
+참고 2 URL - https://docs.python.org/ko/3/howto/logging.html#basic-logging-tutorial
+참고 3 URL - https://docs.aws.amazon.com/ko_kr/lambda/latest/dg/python-logging.html
+참고 4 URL - https://wikidocs.net/84432
+참고 5 URL - https://wikidocs.net/123324
+참고 6 URL - https://velog.io/@jeongpar/Python-Logging-%EC%82%AC%EC%9A%A9%EB%B2%95
+참고 7 URL - https://aoc55.tistory.com/10
+참고 8 URL - https://chuun92.tistory.com/7
+참고 9 URL - https://malwareanalysis.tistory.com/527
+참고 10 URL - https://github.com/sungwook-practice/python_logging_logger.git
+참고 11 URL - https://velog.io/@qlgks1/python-python-logging-%ED%95%B4%EB%B6%80
+
+* logging 모듈 Stack Traces 캡쳐 exc_info=True
+참고 URL - https://docs.python.org/ko/3.7/library/logging.html
+참고 2 URL - https://tjlog-tistory.tistory.com/82
+
+챗봇 전역 로그 모듈 구현 순서 및 로그 기록 방법
+​1. instance 설정 - log(로그) instance 설정
+2. 기록할 log level 지정 - 로그 레벨 지정 (DEBUG, INFO, WARNING, ERROR, CRICTICAL)
+3. formatter 생성 - log(로그)를 저장(기록)할 포맷(format) 지정 
+4. handler 생성 - log(로그)를 담을 수 있는 핸들러(handler) 생성 -> log(로그)를 콘솔창에 출력 할건지? 아니면 파일에 저장 할건지? 이런 저장하는 핸들러(handler) 생성하기
+5. handler에 formatter 설정(할당) - 3번에서 지정한 포맷(format)을 핸들러(handler)에 설정(할당)
+6. 입력받는 instance에 handler 추가 - 3번에서 지정한 포맷(format)을 핸들러(handler)에 설정(할당)한 이후 해당 핸들러(handler)를 log(로그) instance에 추가하여 사용자를 통해 "입력받는 값"을 핸들러(콘솔 또는 파일)로 가져올 수 있도록 또 설정하기 
+7. 다른 파이썬 스크립트 파일에서 챗봇 전역 로그 모듈 import 처리 - from modules.log import logger   # 챗봇 전역 로그 객체 (logger)  
+8. 원하는 지점에 로그 기록하기 (예) logger.info("[테스트] ~~~"), logger.error(f"[테스트] 오류 - {error_msg}") 등등...
+
+* 로그 포맷(format) formatter
+|     이름    |   포멧           |   설명   |
+| asctime     | %(asctime)s     | 날짜 시간, ex) 2021.04.10 11:21:48,162
+| created     | %(created)f     | 생성 시간 출력
+| filename    | %(filename)s    | 파일명
+| funcName    | %(funcName)s    | 함수명
+| levelname   | %(levelname)s   | 로그 레벨(DEBUG, INFO, WARNING, ERROR, CRITICAL)
+| levelno     | %(levelno)s     | 로그 레벨 수치화해서 출력(10, 20, 30, …)
+| lineno      | %(lineno)d      | 소스의 라인 넘버
+| module      | %(module)s      | 모듈 이름
+| msecs       | %(msecs)d       | 로그 생성 시간에서 밀리세컨드 시간 부분만 출력
+| message     | %(message)s     | 로그 메시지
+| name        | %(name)s        | 로그 이름
+| pathname    | %(pathname)s    | 소스 경로
+| process     | %(process)d     | 프로세스(Process) ID
+| processName | %(processName)s | 프로세스 이름
+| thread      | %(thread)d      | Thread ID
+| threadName  | %(threadName)s  | Thread Name
+
+* 로그 레벨 종류 
+|   Level   |   Value   |   When to use
+|   DEBUG   |     10    | (주로 문제 해결을 할때 필요한) 자세한 정보. - 개발 과정에서 오류 원인 파악하고자 할 때 사용
+|   INFO    |     20    | 작업이 정상적으로 작동하고 있다는 메시지. 
+|  WARNING  |     30    | 예상하지 못한 일이 발생하거나, 발생 가능한 문제점을 명시. (e.g 'disk space low') 작업은 정상적으로 진행.
+|   ERROR   |     40    | 프로그램이 함수를 실행하지 못 할 정도의 심각한 문제.
+| CRICTICAL |     50    | 프로그램이 동작할 수 없을 정도의 심각한 문제. 
+"""
